c370bc0f9e7217f15ce8d9eed146ec5f297c05cf
Ryu Yamada[alias] <a2sin2a2ko@kyoto-y.work>
2020-09-01 00:39:21 +0900

    add : 改善はした。計算はおかしい

--- lorenz-96.cpp
+++ lorenz-96.cpp
@@ -29,7 +29,12 @@ std::vector<double> muler(std::vector<double> v1, double s){
     }
     return v3;
 }
-
+void printer(std::vector<double> v){
+    rep(i, v.size()){
+        std::cout << v[i] << ",";
+    }
+    std::cout << std::endl;
+}
 std::vector<double> lorenz96(std::vector<double> v,std::vector<double> k,double t, double F){
     //引数　x,kとタイムステップ
     //返り値 f(v, t)
@@ -39,9 +44,11 @@ std::vector<double> lorenz96(std::vector<double> v,std::vector<double> k,double 
     std::vector<double> v_out(N, 0.0);
     //v_in=v + k * t
     v_in = adder(v, muler(k,t));
+    //std::cout << "v_in time step" << t << std::endl;
+    //printer(v_in);
     //微分方程式の右辺を計算
     rep(i, N){
-        if(i = N - 1){
+        if(i == N - 1){
             ia = 0;
         }else{
             ia = i + 1;
@@ -58,18 +65,15 @@ std::vector<double> lorenz96(std::vector<double> v,std::vector<double> k,double 
         }
         v_out[i] = (v_in[ia] - v_in[ib]) * v_in[ic] - v_in[i] + F;
     }
+    //std::cout << "v_out time step" << t << std::endl;
+    //printer(v_out);
     return v_out;
 }
-void printer(std::vector<double> v){
-    rep(i, v.size()){
-        std::cout << v[i] << ",";
-    }
-    std::cout << std::endl;
-}
+
 int main(){
     //定義
     int N = 40;
-    double F = 8;
+    double F = 8.0;
     double t_max = 1000;
     double dt = 0.2;
     std::vector<double> x(N,1.0);
@@ -78,15 +82,13 @@ int main(){
     std::vector<double> k3(N,0.0);
     std::vector<double> k4(N,0.0);
     // loop
-    for(double t=0.0;t+=dt;t<=t_max){
+    for(double t=0.0;t<=t_max;t+=dt){
         k1 = lorenz96(x, x, 0, F);//２つ目のxはダミーで0によって消える
         k2 = lorenz96(x, k1, dt/2, F);
         k3 = lorenz96(x, k2, dt/2, F);
         k4 = lorenz96(x, k3, dt, F);
         x = adder(x, muler(adder(adder(k1, k4),muler(adder(k2, k3), 2.0)), dt/6));
         printer(x);
-        printer(k1);
-
     }
     return 0;
 }
