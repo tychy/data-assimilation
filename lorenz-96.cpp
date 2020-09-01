4eabd6f2b2a77e86013c1efcaa5630036edd05a5
Ryu Yamada[alias] <a2sin2a2ko@kyoto-y.work>
2020-09-01 12:25:01 +0900

    add : 人類にとっての大きな一歩

--- lorenz-96.cpp
+++ lorenz-96.cpp
@@ -30,40 +30,23 @@ std::vector<double> muler(std::vector<double> v1, double s){
     return v3;
 }
 void printer(std::vector<double> v){
-    rep(i, v.size()){
+    rep(i, v.size()-1){
         std::cout << v[i] << ",";
     }
-    std::cout << std::endl;
+    std::cout << v[v.size()-1]<< std::endl;
 }
 std::vector<double> lorenz96(std::vector<double> v,std::vector<double> k,double t, double F){
     //引数　x,kとタイムステップ
     //返り値 f(v, t)
     int N = v.size();
-    int ia, ib, ic;
     std::vector<double> v_in(N, 0.0);
     std::vector<double> v_out(N, 0.0);
-    //v_in=v + k * t
     v_in = adder(v, muler(k,t));
     //std::cout << "v_in time step" << t << std::endl;
     //printer(v_in);
     //微分方程式の右辺を計算
     rep(i, N){
-        if(i == N - 1){
-            ia = 0;
-        }else{
-            ia = i + 1;
-        }
-        if(i == 0){
-            ib = N - 2;
-            ic = N - 1;
-        }else if(i == 1){
-            ib = N - 1;
-            ic = 0;
-        }else{
-        ib = i - 2;
-        ic = i - 1;
-        }
-        v_out[i] = (v_in[ia] - v_in[ib]) * v_in[ic] - v_in[i] + F;
+        v_out[i] = (v_in[(i+1)%N] - v_in[(i - 2 + N) % N]) * v_in[(i - 1 + N)%N] - v_in[i] + F;
     }
     //std::cout << "v_out time step" << t << std::endl;
     //printer(v_out);
@@ -72,22 +55,39 @@ std::vector<double> lorenz96(std::vector<double> v,std::vector<double> k,double 
 
 int main(){
     //定義
-    int N = 40;
+    int N = 10;
     double F = 8.0;
-    double t_max = 1000;
+    double t_max = 1;
     double dt = 0.2;
-    std::vector<double> x(N,1.0);
+    std::vector<double> x(N,F);//平衡
+    //誤差
+    x[0] += 0.01;
     std::vector<double> k1(N,0.0);
     std::vector<double> k2(N,0.0);
     std::vector<double> k3(N,0.0);
     std::vector<double> k4(N,0.0);
+    std::vector<double> kmid(N,0.0);
+
     // loop
     for(double t=0.0;t<=t_max;t+=dt){
+        std::cout << "-------------STEP" << t/dt<< "start-------------"<< std::endl; 
         k1 = lorenz96(x, x, 0, F);//２つ目のxはダミーで0によって消える
         k2 = lorenz96(x, k1, dt/2, F);
         k3 = lorenz96(x, k2, dt/2, F);
         k4 = lorenz96(x, k3, dt, F);
-        x = adder(x, muler(adder(adder(k1, k4),muler(adder(k2, k3), 2.0)), dt/6));
+        kmid = muler(adder(adder(k1, k4),muler(adder(k2, k3), 2.0)), dt/6);
+        x = adder(x, kmid);
+        std::cout << "k1" << std::endl; 
+        printer(k1);
+        std::cout << "k2" << std::endl; 
+        printer(k2);
+        std::cout << "k3" << std::endl; 
+        printer(k3);
+        std::cout << "k4" << std::endl; 
+        printer(k4);
+        std::cout << "kmid" << std::endl; 
+        printer(kmid);
+        std::cout << "x" << std::endl; 
         printer(x);
     }
     return 0;
