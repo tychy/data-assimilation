720801104cc9b8038f6b66daa51702617f76789a
Ryu Yamada[alias] <a2sin2a2ko@kyoto-y.work>
2020-09-03 20:41:25 +0900

    add : プロットできたよ

--- q2/plot_error.py
+++ q2/plot_error.py
@@ -2,15 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def parse_dat(fname):
+    x = []#each contain numpy list
+    with open(fname) as f:
+        line = f.readline()
+        data = np.array([],dtype=float)
+        while True:
+            line = f.readline()
+            if line.find("time") > -1:
+                x.append(data)
+                data = np.array([],dtype=float)
+                continue
+            if not line:
+                x.append(data)
+                break
+            data = np.append(data, float(line))
+    return x
+
+
 def main():
     fig = plt.figure()
-    x = np.loadtxt('error1.dat')
-    x_mean = np.zeros((x.shape[0],), dtype=float)
-    for i in range(5):
-        j = i + 1
-        x = np.loadtxt('error' + str(j) + '.dat')
-        x_mean += x
-    plt.plot(x_mean/5, label='mean')
+    x = parse_dat('error.dat')
+    x_mean = np.zeros((x[0].shape[0],), dtype=float)
+    for i in range(len(x)):
+        x_mean += x[i]
+    plt.plot(x_mean/len(x), label='mean')
+    plt.grid(which='major',color='black',linestyle='-')
     plt.legend()
     plt.savefig('error.png')
 
