43ab6e588b312a987cfd053cddcabd5fd2cbd3c1
Ryu Yamada[alias] <a2sin2a2ko@kyoto-y.work>
2020-09-03 20:19:36 +0900

    add : main

--- q2/plot_error.py
+++ q2/plot_error.py
@@ -2,13 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig = plt.figure()
-x = np.loadtxt('error1.dat')
-x_mean = np.zeros((x.shape[0],), dtype=float)
-for i in range(5):
-    j = i + 1
-    x = np.loadtxt('error' + str(j) + '.dat')
-    x_mean += x
-plt.plot(x_mean/5, label='mean')
-plt.legend()
-plt.savefig('error.png')
+def main():
+    fig = plt.figure()
+    x = np.loadtxt('error1.dat')
+    x_mean = np.zeros((x.shape[0],), dtype=float)
+    for i in range(5):
+        j = i + 1
+        x = np.loadtxt('error' + str(j) + '.dat')
+        x_mean += x
+    plt.plot(x_mean/5, label='mean')
+    plt.legend()
+    plt.savefig('error.png')
+
+
+if __name__ == "__main__":
+    main()
