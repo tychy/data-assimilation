4a8232efeb5a1bd577b705b1e37c54fa30fed3b8
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-09-29 11:58:11 +0900

    add : rm tqdm

--- py_KF/kf_kansoku.py
+++ py_KF/kf_kansoku.py
@@ -11,18 +11,19 @@ from kflib import *
 plt.rcParams["figure.figsize"] = (20, 6)
 N = 40
 F = 8
+x0 = F * np.ones(N)  # Initial state (equilibrium)
+x0[0] += 0.01  # Add small perturbation to the first variable
+x = l96_step(x0, 10)
+x_one = l96_step(x0, 73.0)  # spin up
+x = l96_step(x_one[-1], 73.0)
+x_last = x_one[-1]
+
 if not (os.path.exists("datawithnoise.txt") and os.path.exists("gendata.txt")):
-    x0 = F * np.ones(N)  # Initial state (equilibrium)
-    x0[0] += 0.01  # Add small perturbation to the first variable
-    x = l96_step(x0, 10)
-    x_one = l96_step(x0, 73.0)  # spin up
-    x = l96_step(x_one[-1], 73.0)
     data = x[::5]
     np.savetxt("gendata.txt", data)
     random_ls = np.random.randn(data.shape[0], x[::5].shape[1])
     datawithnoise = data + random_ls
     np.savetxt("datawithnoise.txt", datawithnoise)
-    x_last = x_one[-1]
 else:
     data = np.loadtxt("gendata.txt")
     datawithnoise = np.loadtxt("datawithnoise.txt")
@@ -35,7 +36,7 @@ def find_best_alpha(del_num):
     kf_before_ls, kf_after_ls, x_ls = [], [], []
     min_alpha = 10
     min_idx = 0.0
-    for item in tqdm(hani):
+    for item in hani:
         try:
             kf_out = kf(
                 x_last, data, datawithnoise, param=item, step=step, del_num=del_num
