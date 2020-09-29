0aeab4c7405bc01c2cdda4df2e01b1f9774347ca
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-09-29 15:51:17 +0900

    add :po 法動いた

--- py_KF/po_kf.py
+++ py_KF/po_kf.py
@@ -13,12 +13,12 @@ def po_forecast(xa):
     return l96_step(xa, 0.01)[-1]
 
 
-def po(x_last, data, datawithnoise, step=100, param=1.0, del_num=0, m=8):
+def po(x_last, data, datawithnoise, step=100, del_num=0, m=8):
     I = np.eye(N)
-    xf_ls = []
-    xa_ls = np.array([
-        l96_step(x_last, 0.05 * int(np.random.randint(1, 1000)))[-1] for _ in range(m)
-    ])  # 初期値を作成
+    xa_ls = np.array(
+        [l96_step(x_last, 0.05 * int(np.random.randint(1, 1000)))[-1] for _ in range(m)]
+    )  # 初期値を作成
+    xf_ls = xa_ls
 
     Z = np.zeros_like(I)
     H_del = np.eye(N)
@@ -34,26 +34,30 @@ def po(x_last, data, datawithnoise, step=100, param=1.0, del_num=0, m=8):
     for i in range(step * 5):
         if i % 5 == 0:
             H = H_del
-            R = np.eye(H.shape[0])
+            R = np.eye(H.shape[0]) * (m - 1)
             yt = data[i // 5]
             y = np.array(datawithnoise[i // 5])
             y = np.delete(y, del_key)
 
             xf_mean = np.zeros(N)
-            xf_ls = []
             for k in range(m):
                 xa = xa_ls[k]
                 xf = po_forecast(xa)
-                xf_ls.append(xf)
+                xf_ls[k] = xf
                 xf_mean = xf_mean + xf
             xf_mean = xf_mean / m
-            xf_ls = np.array(xf)
+            xf_ls = np.array(xf_ls)
             xferror_before_assim.append(np.sqrt(mean_squared_error(yt, xf)))
-            dxf = xf_ls - xf_mean * np.ones(N)
-            dyf = H @ xf - H @ xf_mean
-            K = dxf @dyf.T np.linalg.inv(dyf @dxf.T+(m-1)*I)
-            xa_ls = xf + K @ (y-H@xf)
-            xferror_after_assim.append(np.sqrt(mean_squared_error(yt, xa)))
+            dxf = (xf_ls - xf_mean * np.ones(N)).T
+            dyf = np.array([H @ xf_ls[idx] - H @ xf_mean for idx in range(m)]).T
+            K = dxf @ dyf.T @ np.linalg.inv(dyf @ dxf.T + R)
+            scpre = 0
+            for k in range(m):
+                xf = xf_ls[k]
+                xferror_before_assim.append(np.sqrt(mean_squared_error(yt, xf)))
+                xa = xf + K @ (y + np.random.randn() - H @ xf)
+                xa_ls[k] = xa
+                xferror_after_assim.append(np.sqrt(mean_squared_error(yt, xa)))
         else:
             H = Z_del
             for k in range(m):
---
+++ py_KF/po_run.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+import random
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (20, 6)
+from scipy.integrate import odeint
+from sklearn.metrics import mean_squared_error
+from kflib import *
+from params import *
+from po_kf import *
+
+N = 40
+F = 8
+x0 = F * np.ones(N)  # Initial state (equilibrium)
+x0[0] += 0.01  # Add small perturbation to the first variable
+x = l96_step(x0, 10)
+x_one = l96_step(x0, 73.0)  # spin up
+x = l96_step(x_one[-1], 73.0)
+x_last = x_one[-1]
+
+if not (os.path.exists("datawithnoise.txt") and os.path.exists("gendata.txt")):
+    data = x[::5]
+    np.savetxt("gendata.txt", data)
+    random_ls = np.random.randn(data.shape[0], x[::5].shape[1])
+    datawithnoise = data + random_ls
+    np.savetxt("datawithnoise.txt", datawithnoise)
+else:
+    data = np.loadtxt("gendata.txt")
+    datawithnoise = np.loadtxt("datawithnoise.txt")
+
+m = 40
+xferror_before_assim, xferror_after_assim = po(
+    x_last, data, datawithnoise, step=100, del_num=0, m=m
+)
+plt.plot(xferror_after_assim)
+plt.savefig("po{}.png".format(m))
